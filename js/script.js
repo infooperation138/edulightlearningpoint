d6a6f9d40b1e6d061c40346c605984213e8d14f1
infooperation138 <info.operation138@gmail.com>
2026-07-06 19:30:37 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Edulight Learning Point
+
+console.log("Website Loaded Successfully");
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    console.log("Edulight Learning Point Ready");
+
+});
