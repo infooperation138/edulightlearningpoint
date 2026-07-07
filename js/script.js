ba13d16a8887df68bec1bec22958331ff325249b
infooperation138 <info.operation138@gmail.com>
2026-07-07 18:59:59 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,3 +7,22 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("Edulight Learning Point Ready");
 
 });
+document.addEventListener("DOMContentLoaded", function () {
+
+    const form = document.getElementById("admissionForm");
+
+    if(form){
+
+        form.addEventListener("submit", function(e){
+
+            e.preventDefault();
+
+            document.getElementById("successMessage").classList.remove("d-none");
+
+            form.reset();
+
+        });
+
+    }
+
+});
