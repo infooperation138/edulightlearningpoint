b558b7c03654c66268910625887dd41ca6c53590
infooperation138 <info.operation138@gmail.com>
2026-07-21 13:04:54 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,23 +6,42 @@ document.addEventListener("DOMContentLoaded", function () {
 
     console.log("Edulight Learning Point Ready");
 
-});
-document.addEventListener("DOMContentLoaded", function () {
-
     const form = document.getElementById("admissionForm");
 
-    if(form){
+    if (!form) return;
 
-        form.addEventListener("submit", function(e){
+    form.addEventListener("submit", function (e) {
 
-            e.preventDefault();
+        e.preventDefault();
+
+        const data = {
+            name: form.elements["name"].value,
+            mobile: form.elements["mobile"].value,
+            email: form.elements["email"].value,
+            university: form.elements["university"].value,
+            course: form.elements["course"].value
+        };
+
+        fetch("https://script.google.com/macros/s/AKfycbyeehpAweTytrnjV4QRJtmxCeeFAacv4hdDxM-1Ub227UyLeBCwYu3WF5YwgFmwcsE/exec", {
+            method: "POST",
+            body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .then(result => {
 
             document.getElementById("successMessage").classList.remove("d-none");
 
             form.reset();
 
+        })
+        .catch(error => {
+
+            console.error(error);
+
+            alert("Something went wrong!");
+
         });
 
-    }
+    });
 
 });
