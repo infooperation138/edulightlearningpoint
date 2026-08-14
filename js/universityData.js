47b2f5baa8750a6f734f02744f9db2b858ac811d
infooperation138 <info.operation138@gmail.com>
2026-08-14 17:48:34 +0530

    Update universityData.js

--- js/universityData.js
+++ js/universityData.js
@@ -39,3 +39,41 @@ PG:{
 
 },
     
+"Amity University (Online)":{
+
+UG:{
+"BBA":["General","Data Analytics","Travel & Tourism Management"],
+"BCA":["Data Science","Data Engineering","Financial Technology & AI","Data Analytics","Cloud Security","Software Engineering"],
+"B.Com":["General","Hons","International Finance & Accounting"],
+"BA":["General","Hindi","Tamil","Kannada","Telugu","Hindi","Sociology","Political Science","English","Economics"]
+},
+
+PG:{
+"MBA":[
+"Data Science",
+"Business Analytics",
+"International Finance",
+"General Management",
+"Digital Marketing Management",
+"Hospital & Healthcare Management",
+"Dual Specialization"
+],
+
+"MCA":[
+"Software Engineering",
+"FinTech & AI",
+"Machine Learning & AI",
+"Cyber Security",
+"Blockchain Technology & Management"
+],
+
+"M.Com":["Financial Management"],
+
+"MA":[
+"JMC",
+"Public Policy & Governance"
+]
+
+}
+
+},
